01a2581300644013965aa32bcf5ee6ece20a851a
vic30004 <victor_993@msn.com>
2020-04-02 20:43:06 -0700

    added a fucntion to toggle the menu

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const toggle = document.getElementById('toggle');
 const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
+
+
+// toggle nav
+
+toggle.addEventListener('click',()=>{
+    document.body.classList.toggle('show-nav')
+})
